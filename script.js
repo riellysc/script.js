ecb87bfd5f67ce51290854a3b587791a142f00bd
riellysc <riellymara010@gmail.com>
2023-02-07 16:04:45 -0300

    ex001JavaScript

---
+++ script.js
@@ -0,0 +1,31 @@
+//ex001
+let x = 1
+console.log(x)
+//ex002
+let nome= "rielly"
+console.log(nome)
+//ex003
+let idade=19
+let estiloMusical="pop"
+console.log(idade,estiloMusical)
+//ex004
+const filme="passageiros"
+const musica="Far"
+console.log(filme)
+console.log(musica)
+//ex005
+function triploDaSoma(a, b) {
+    return 3 * (a + b);
+  }
+  console.log(triploDaSoma(3, 2));
+  //ex006
+  let bool = false;
+
+  function verificarVerdade() {
+    if (bool == true) {
+      return console.log("é verdadeira");
+    } else {
+      return console.log("é falsa");
+    }
+  }
+  verificarVerdade();
